75b3eee675cdff73eb46fbe1aabddca2bca41d76
jx1 <jx1@email.com>
2017-09-07 15:26:57 +0800

    create program

---
+++ app.js
@@ -0,0 +1,44 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    }),
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  //当用户离开微信，小程序进入了后台；当再次进入微信或再次打开小程序，又会从后台进入前台
+  onShow: function (path, scene){
+    console.log("im back");
+    console.log(path,"...", scene);
+  },
+  globalData: {
+    userInfo: null
+  }
+})
